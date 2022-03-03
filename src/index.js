625ccd2d276e026026c6f420927afc0e262b6d4c
Thaiane Cristine <thaiane.cristine@gruposbf.com.br>
2022-03-03 11:35:22 -0300

    meu primeiro commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Square = () => {
+  const [value, setValue] = useState(null) 
+  const handleClick = () => {
+    setValue("x")
+  };
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Board = () => {
+  const renderSquare = () => {
+    return <Square />;
+  };
+
+  const status = "Next player: X";
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+      </div>
+      <div className="board-row">
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+      </div>
+      <div className="board-row">
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+      </div>
+    </div>
+  );
+};
+
+const Game = () => {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
+};
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
