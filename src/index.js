c339a1ff8d41b4492344ad8663d21fe899e895b4
Thaiane Cristine <thaiane.cristine@gruposbf.com.br>
2022-03-03 13:54:32 -0300

    criando componente square

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import Square from "./componentes/square";
 import "./index.css";
-
-const Square = () => {
-  const [value, setValue] = useState(null) 
-  const handleClick = () => {
-    setValue("x")
-  };
-  return (
-    <button className="square" onClick={handleClick}>
-      {value}
-    </button>
-  );
-};
 
 const Board = () => {
   const renderSquare = () => {
