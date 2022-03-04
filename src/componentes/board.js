684ebe1133452581567cb64fa40b435bf3c004ab
Thaiane Cristine <thaiane.cristine@gruposbf.com.br>
2022-03-04 12:32:37 -0300

    Criando componente Game e arrumando indentação

--- src/componentes/board.js
+++ src/componentes/board.js
@@ -1,34 +1,33 @@
 import React from "react";
 import Square from "./square";
 
-
 const Board = () => {
-    const renderSquare = () => {
-      return <Square />;
-    };
-  
-    const status = "Next player: X";
-  
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-        </div>
-        <div className="board-row">
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-        </div>
-        <div className="board-row">
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-        </div>
-      </div>
-    );
+  const renderSquare = () => {
+    return <Square />;
   };
 
-  export default Board;
+  const status = "Next player: X";
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+      </div>
+      <div className="board-row">
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+      </div>
+      <div className="board-row">
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+      </div>
+    </div>
+  );
+};
+
+export default Board;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Board from "./componentes/board";
+import Game from "./componentes/game";
 import "./index.css";
-
-const Game = () => {
-  return (
-    <div className="game">
-      <div className="game-board">
-        <Board />
-      </div>
-      <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
-      </div>
-    </div>
-  );
-};
-
-// ========================================
 
 ReactDOM.render(<Game />, document.getElementById("root"));
